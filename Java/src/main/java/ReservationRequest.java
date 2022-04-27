e41fead505c8ec40eb3c07f1c947913505b3fa82
aalberti <aa@antoinealberti.com>
2022-04-27 17:33:38 +0200

    Migrate to records

--- Java/src/main/java/ReservationRequest.java
+++ Java/src/main/java/ReservationRequest.java
@@ -1,11 +1,3 @@
 
-public class ReservationRequest {
-	public final String trainId;
-    public final int seatCount;
-
-    public ReservationRequest(String trainId, int seatCount) {
-		this.trainId = trainId;
-        this.seatCount = seatCount;
-    }
-
+public record ReservationRequest (String trainId, int seatCount) {
 }
