6b6d66a144accdfe3efbfbb96e977dbe966975f7
aalberti <aa@antoinealberti.com>
2022-04-27 17:36:24 +0200

    Inject dependencies

--- Java/src/main/java/TicketOffice.java
+++ Java/src/main/java/TicketOffice.java
@@ -1,5 +1,15 @@
+import service.booking.BookingService;
+import service.train.TrainService;
+
 public class TicketOffice {
-    
+    private final TrainService trainService;
+    private final BookingService bookingService;
+
+    public TicketOffice(TrainService trainService, BookingService bookingService) {
+        this.trainService = trainService;
+        this.bookingService = bookingService;
+    }
+
     public Reservation makeReservation(ReservationRequest request) {
 		//TODO: implement this code!
 		return null;
--- Java/src/test/java/TicketOfficeTest.java
+++ Java/src/test/java/TicketOfficeTest.java
@@ -1,11 +1,20 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+import service.booking.BookingService;
+import service.train.TrainService;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class TicketOfficeTest {
-    
     @Test
     public void reserveSeats() {
-        // TODO: Write this code!
+        final var trainService = new TrainService();
+        final var bookingService = new BookingService();
+        final var ticketOffice = new TicketOffice(trainService, bookingService);
 
+        final var reservation = ticketOffice.makeReservation(new ReservationRequest(null, 0));
+
+        assertThat(reservation,
+                is(new Reservation(null, null, null)));
     }
 }
